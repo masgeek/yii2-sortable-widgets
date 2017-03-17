07bdb2318f278d3d874c4eb8065aa76158f40c8b
Munywele B <barsamms@gmail.com>
2017-03-18 00:23:46 +0300

    Removed remote sortable file call
    will replace it with a bower call

--- assets/RubaxaAsset.php
+++ assets/RubaxaAsset.php
@@ -9,7 +9,7 @@ class RubaxaAsset extends AssetBundle
     public $sourcePath = '@vendor/fatelord/yii2-sortable-widgets/assets/files';
 
     public $js = [
-        '//cdnjs.cloudflare.com/ajax/libs/Sortable/1.2.1/Sortable.js',
+        //'//cdnjs.cloudflare.com/ajax/libs/Sortable/1.2.1/Sortable.js',
         'js/jquery.binding.js',
     ];
 
